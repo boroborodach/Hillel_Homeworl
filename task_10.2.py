8f909cc3cbdcd620c7e18a7ed93d00e62b39bc8b
boroborodach <borodach19941901@gmail.com>
2026-04-02 10:59:59 +0300

    task 10.3 done

--- task_10.2.py
+++ task_10.2.py
@@ -7,7 +7,6 @@ def first_word(text):
         return match.group()
     return None
 
-
 assert first_word("Hello world") == "Hello", 'Test1'
 assert first_word("greetings, friends") == "greetings", 'Test2'
 assert first_word("don't touch it") == "don't", 'Test3'
