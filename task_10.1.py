dcb3331b6d5d6f698bc01c2751915e0b6034e480
boroborodach <borodach19941901@gmail.com>
2026-04-02 10:40:40 +0300

    task 10.1 done

---
+++ task_10.1.py
@@ -0,0 +1,23 @@
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+    res = begin
+    for i in range(end):
+        if i == 0:
+            yield res
+        else:
+            res = func(res)
+            yield res
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
