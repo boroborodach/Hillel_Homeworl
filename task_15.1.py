513c284b821b0d301cb312d607830b4abd323aed
boroborodach <borodach19941901@gmail.com>
2026-04-22 10:54:03 +0300

    task 15.1 done

---
+++ task_15.1.py
@@ -0,0 +1,97 @@
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+    def __str__(self):
+        return f"First name: {self.first_name}; \nLast name: {self.last_name}; \nAge: {self.age}; \nGender: {self.gender}."
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.record_book = record_book
+
+
+    def __str__(self):
+        return f"First name: {self.first_name}; \nLast name: {self.last_name}; \nAge: {self.age}; \nGender: {self.gender}."
+
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+
+    def add_student(self, student):
+        if len(self.group) < 10:
+            self.group.add(student)
+        else:
+            raise Exception("Group student limit exceeded")
+
+
+    def delete_student(self, last_name):
+        for e in self.group:
+            if e.last_name == last_name:
+                self.group.remove(e)
+                return
+
+
+    def find_student(self, last_name):
+        for e in self.group:
+            if e.last_name == last_name:
+                return e
+        return None
+
+
+    def __str__(self):
+        all_students = ''
+        ...
+        return f'Number:{self.number}\n {all_students} '
+
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st3 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st4 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st5 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st6 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st7 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st8 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st9 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st10 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st11 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+
+gr = Group('PD1')
+
+try:
+    gr.add_student(st1)
+    gr.add_student(st2)
+    gr.add_student(st3)
+    gr.add_student(st4)
+    gr.add_student(st5)
+    gr.add_student(st6)
+    gr.add_student(st7)
+    gr.add_student(st8)
+    gr.add_student(st9)
+    gr.add_student(st10)
+    gr.add_student(st11)
+except Exception as err:
+    print(err)
+
+print(gr)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод пошуку повинен повертати екземпляр'
+
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+
+gr.delete_student('Taylor')  # No error!
